adb8b7f5ffb8c13df423084a24455736b711fbca
noblekuz <noblekuz@gmail.com>
2025-02-28 02:47:24 +0100

    Phone verification added

--- js/script.js
+++ js/script.js
@@ -26,11 +26,33 @@ import config from './store/config.js';
 document.addEventListener("DOMContentLoaded", () => {
     document.getElementById("register_newUser_btn").addEventListener("click", register_newUser);
 });
+document.addEventListener("DOMContentLoaded", () => {
+    document.getElementById("sendPhone_btn").addEventListener("click", verifyPhone);
+});
+
+document.addEventListener("DOMContentLoaded", () => {
+    document.getElementById("verify_code_btn").addEventListener("click", submitPhoneVerificationCode);
+});
+
+document.addEventListener("DOMContentLoaded", () => {
+    document.getElementById("send_verification_code_again_btn").addEventListener("click", verifyPhone);
+});
+
+document.addEventListener("DOMContentLoaded", function () {
+        let newUsers_phone = localStorage.getItem("reg_phone");
+        document.getElementById("displayPhone").innerText = `+49 ${newUsers_phone}`;
+        console.log(temp_phone)    
+});
+
+document.addEventListener("DOMContentLoaded", () => {
+    document.getElementById("displayPhone").textContent = phoneNumber;
+});
+
+let server_url = config.API_URL
 
 
 async function register_newUser() {
-    let server_url = config.API_URL
-
+    
     let username = document.getElementById("signup_username").value;
     let email = document.getElementById("signup_email").value;
     let password = document.getElementById("signup_pass").value;
@@ -41,6 +63,73 @@ async function register_newUser() {
         body: JSON.stringify({ "username":username, "email":email, "password":password })
     });
 
-    const data = await response.json();
-    alert("login-message" +JSON.stringify(data));
+    const res = await response.json();
+    if(res.user && res.user.email == email && res.userProfile.username == username){
+       alert("Kaish "+username+ " your account is almost ready.") 
+       localStorage.setItem("reg_id", res.userProfile._id);
+       window.location.href = './verifiction.html';
+    }else{
+        if(!res.user){
+            alert("fe[err_100]: failed to create account")
+        }
+        if(res.user && !res.userProfile.username){
+            alert("fe[info_100]: failed to intiialize profile. You can do this later")
+        }
+    }
+    alert(JSON.stringify(data.user.email));
+}
+
+async function verifyPhone() {
+    let phone = document.getElementById("verification_phone_signup").value;
+    function isValidPhoneNumber(phone) {
+        const phoneRegex = /^\+?\d{7,15}$/; // Supports optional '+' and 7-15 digits
+        return true//phoneRegex.test(phone);
+    }
+    
+    if(isValidPhoneNumber(phone)){
+        const response = await fetch(`${server_url}/user/send-code_to_phone`, {
+            method: "POST",
+            headers: { "Content-Type": "application/json" },
+            body: JSON.stringify({ "phone":phone })
+        });
+
+        localStorage.setItem("reg_phone", phone);
+        const res = await response.json();
+
+        if(res.code ==100){
+            alert(JSON.stringify(res.message))
+            window.location.href = './verify.html';
+        }else{
+            alert("failed to save verification code")
+        }
+    }else{
+        alert("Invalid Phone number")
+    }
+}
+
+async function submitPhoneVerificationCode() {
+    let code_digit_1 = document.getElementById("verCode_1").value;
+    let code_digit_2 = document.getElementById("verCode_2").value;
+    let code_digit_3 = document.getElementById("verCode_3").value;
+    let code_digit_4 = document.getElementById("verCode_4").value;
+
+    let full_code = code_digit_1+code_digit_2+code_digit_3+code_digit_4
+
+    let newUsers_phone = localStorage.getItem("reg_phone");
+    let newUser_id = localStorage.getItem("reg_id");
+
+    const response = await fetch(`${server_url}/user/verify-code`, {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({ "phone":newUsers_phone, "code": full_code, "id": newUser_id })
+    });
+
+    const res = await response.json();
+    console.log(JSON.stringify(res))
+    /* if(res.code){
+        alert(JSON.stringify(res.message))
+        window.location.href = './verify.html';
+    }else{
+        alert("failed to create verification code")
+    } */
 }
